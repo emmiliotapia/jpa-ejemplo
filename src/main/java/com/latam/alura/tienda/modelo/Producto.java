c1bff4beb0b3e7cfde736965155a0c3b07dd1f32
emmiliotapia <emmiliotapia@gmail.com>
2023-09-12 16:25:02 -0700

    Se agregaron atributos en Producto: Categorias & FechaDeRegistro

--- src/main/java/com/latam/alura/tienda/modelo/Producto.java
+++ src/main/java/com/latam/alura/tienda/modelo/Producto.java
@@ -1,11 +1,13 @@
 package com.latam.alura.tienda.modelo;
 
 import java.math.BigDecimal;
+import java.time.LocalDate;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 @Entity
@@ -18,6 +20,19 @@ public class Producto {
 	private String nombre;
 	private String descripcion;
 	private BigDecimal precio;
+	private LocalDate fechaDeRegistro = LocalDate.now();
+	
+	@ManyToOne
+	private Categoria categoria;
+	
+	public Producto(String nombre, String descripcion, BigDecimal precio, Categoria categoria) {
+		super();
+		this.nombre = nombre;
+		this.descripcion = descripcion;
+		this.precio = precio;
+		this.categoria = categoria;
+	}
+
 	public Long getId() {
 		return id;
 	}
--- src/main/java/com/latam/alura/tienda/prueba/RegistroDeProducto.java
+++ src/main/java/com/latam/alura/tienda/prueba/RegistroDeProducto.java
@@ -3,23 +3,31 @@ package com.latam.alura.tienda.prueba;
 import java.math.BigDecimal;
 
 import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
-
+import com.latam.alura.tienda.dao.ProductoDao;
+import com.latam.alura.tienda.modelo.Categoria;
 import com.latam.alura.tienda.modelo.Producto;
+import com.latam.alura.tienda.utils.JPAUtils;
 
 public class RegistroDeProducto {
 
 	public static void main(String[] args) {
-		Producto celular =new Producto();
-		celular.setNombre("Samsung");
-		celular.setDescripcion("telefono usado");
-		celular.setPrecio(new BigDecimal("1000"));
 		
-		EntityManagerFactory factory = Persistence.createEntityManagerFactory("tienda");
-		EntityManager em = factory.createEntityManager();
+		Categoria celulares = new Categoria("CELULARES");
+		
+		Producto celular = new Producto(
+				"Samsung",
+				"telefono usado",
+				new BigDecimal("1000"),
+				celulares);
+		
+		EntityManager em = JPAUtils.getEntityManager();
+		
+		ProductoDao productoDao = new ProductoDao(em);
+		
 		em.getTransaction().begin();
-		em.persist(celular);
+		
+		productoDao.guardar(celular);
+		
 		em.getTransaction().commit();
 		em.close();		
 		
